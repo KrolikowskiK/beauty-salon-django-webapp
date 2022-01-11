e829606c2f6ea148d5f631ca777d4277a8a70f8d
KrolikowskiK <kacperk7373@gmail.com>
2022-01-11 18:51:08 +0100

    feat: add placeholder for date input in appointment form

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,12 @@ from .models import Appointment
 
 
 class AppointmentCreate(forms.ModelForm):
+    date = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={"placeholder": "DD/MM/YYYY HH:MM"}),
+        localize=True,
+        input_formats=["%d/%m/%Y %H:%M"],
+    )
+
     class Meta:
         model = Appointment
         fields = ["employee", "client", "date"]
