cfa88bfa4d3254423e3be0501f39b888768e37ba
Klaudia <klaudiajedzura4@gmail.com>
2022-01-12 23:00:04 +0100

    add name

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import RegisterView, UserLoginView, profile_view
 
 urlpatterns = [
-    path("register/", RegisterView.as_view()),
-    path("login/", UserLoginView.as_view()),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", UserLoginView.as_view(), name="login"),
     path("profile/", profile_view),
 ]
