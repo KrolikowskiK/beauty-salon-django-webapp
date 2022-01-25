f54bc97a0e2a221f7eb578c275841bd9f3ff0b69
KrolikowskiK <kacperk7373@gmail.com>
2022-01-25 22:32:37 +0100

    feat: add authentication for views

--- app/views.py
+++ app/views.py
@@ -2,12 +2,13 @@ from django.views.generic.edit import CreateView, DeleteView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Appointment, Opinion, WorkSchedule
 from .forms import OpinionCreateForm
 
 
-class AppointmentCreateView(CreateView):
+class AppointmentCreateView(LoginRequiredMixin, CreateView):
     model = Appointment
     success_url = reverse_lazy("appointment-list")
     fields = ["employee", "date"]
@@ -17,26 +18,26 @@ class AppointmentCreateView(CreateView):
         return super().form_valid(form)
 
 
-class AppointmentListView(ListView):
+class AppointmentListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return Appointment.objects.filter(client=self.request.user)
 
 
-class AppointmentDeleteView(DeleteView):
+class AppointmentDeleteView(LoginRequiredMixin, DeleteView):
     model = Appointment
     success_url = reverse_lazy("appointment-list")
 
 
-class OpinionCreateView(CreateView):
+class OpinionCreateView(LoginRequiredMixin, CreateView):
     model = Opinion
     form_class = OpinionCreateForm
     success_url = reverse_lazy("intro-view")
 
 
-class WorkScheduleListView(ListView):
+class WorkScheduleListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return WorkSchedule.objects.filter(employee__email=self.request.user.email)
 
 
-class WorkScheduleDetailView(DetailView):
+class WorkScheduleDetailView(LoginRequiredMixin, DetailView):
     model = WorkSchedule
