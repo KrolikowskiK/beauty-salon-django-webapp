6d1c87eae540fabd6f7714852b43ebc21a18065f
KrolikowskiK <kacperk7373@gmail.com>
2022-01-25 21:41:51 +0100

    refactor: change opinion create view to generic view

--- app/views.py
+++ app/views.py
@@ -1,11 +1,9 @@
 from django.views.generic.edit import CreateView, DeleteView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views import View
 from django.urls import reverse_lazy
-from django.shortcuts import redirect, render
 
-from .models import Appointment, WorkSchedule
+from .models import Appointment, Opinion, WorkSchedule
 from .forms import OpinionCreateForm
 
 
@@ -28,19 +26,10 @@ class AppointmentDeleteView(DeleteView):
     success_url = reverse_lazy("appointment-list")
 
 
-class OpinionCreateView(View):
-    template = "app/opinion_form.html"
-
-    def get(self, request):
-        form = OpinionCreateForm(initial={"client": request.user})
-        return render(request, self.template, {"form": form})
-
-    def post(self, request):
-        form = OpinionCreateForm(request.POST or None)
-        if form.is_valid():
-            form.save()
-            return redirect("intro-view")
-        return render(request, self.template, {"form": form})
+class OpinionCreateView(CreateView):
+    model = Opinion
+    form_class = OpinionCreateForm
+    success_url = reverse_lazy("intro-view")
 
 
 class WorkScheduleListView(ListView):
