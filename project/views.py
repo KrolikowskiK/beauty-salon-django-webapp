c73f75498f4a83dbae0f6ec6cec194c53ffbc3df
KrolikowskiK <kacperk7373@gmail.com>
2022-01-25 22:32:04 +0100

    feat: change view to redirect employees
    After login they are redirected to work schedules list.

--- project/views.py
+++ project/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def intro_view(request):
+    if hasattr(request.user, "employee"):
+        return redirect("workschedule-list")
     return render(request, "intro.html")
